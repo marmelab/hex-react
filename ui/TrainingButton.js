630032820fefd076ba9b05ae62c284d8e5dea27d
Chroq <christophe.lecroq@gmail.com>
2019-12-10 15:39:05 +0100

    Use enums to manage mode consts

--- ui/TrainingButton.js
+++ ui/TrainingButton.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Button} from "react-native";
+import {modes} from "./index";
 
 export const TrainingButton = (setMode) => {
     return <Button
         title="Training mode"
         color="#999999"
         onPress={() => {
-            setMode('TRAINING')
+            setMode(modes.TRAINING)
         }}
     />
 };
--- ui/index.js
+++ ui/index.js
@@ -3,13 +3,17 @@ import React, {useState} from 'react';
 import {Board} from "../board";
 import {Menu} from "./Menu";
 
+export const modes = {
+    TRAINING: 'training',
+    CHOOSING: 'choosing'
+};
+
 export const Display = () => {
+    const [mode, setMode] = useState(modes.CHOOSING);
 
-    const [mode, setMode] = useState("CHOOSE");
-
-    if (mode === "TRAINING") {
+    if (mode === modes.TRAINING) {
         return <Board size={6}/>
-    } else if (mode === 'CHOOSE') {
+    } else if (mode === modes.CHOOSING) {
         return <Menu setMode={setMode}/>
     }
 };
