3b1f74d5fe48d3f467a25290a0ea3d64c994b88e
Chroq <christophe.lecroq@gmail.com>
2019-12-12 16:54:14 +0100

    Extract gameboard in his own file

--- board/index.js
+++ board/index.js
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from "react";
-import {Svg, Text} from "react-native-svg";
+import {Svg, Text, Polygon} from "react-native-svg";
 
 import {Cell} from "./Cell";
 import {gridPoints} from "./utils"
 import {fetchIsWon} from "./request";
 import {player1Color, player2Color} from "./theme";
+import {Gameboard} from "./Gameboard";
 
 export const Board = (props) => {
 
@@ -12,7 +13,6 @@ export const Board = (props) => {
     const [isWon, setIsWon] = useState(false);
     const [player, setPlayer] = useState(1);
     const [isLoading, setIsLoading] = useState(false);
-
 
     const handleCellOnPress = (cellNumber, player) => {
         if (isLoading || isWon) {
@@ -37,22 +37,15 @@ export const Board = (props) => {
 
     return (
         <Svg width="500" height="500">
-
             {isWon && (
                 <Text fill={player1Color} fontSize="32" fontWeight="bold" x="175" y="450" textAnchor="middle">
                     You win !
                 </Text>
             )}
-            <Polygon points="0,120 255,120 180,220" fill="#03a9fc" stroke="#222222" strokeWidth="1"/>
-            <Polygon points="180,220 360,345 105,345" fill="#03a9fc" stroke="#222222" strokeWidth="1"/>
-
-            <Polygon points="0,120 180,220 105,345" fill="#f4d032" stroke="#222222" strokeWidth="1"/>
-            <Polygon points="255,120 360,345 180,220" fill="#f4d032" stroke="#222222" strokeWidth="1"/>
-
+            <Gameboard/>
             {gridPoints(20, 75, 22, props.size, props.size).map(({props}, index) => (
                 <Cell {...props} onPress={() => handleCellOnPress(index, player)} value={grid[index]} key={index}/>
             ))}
-
         </Svg>
     );
 };
